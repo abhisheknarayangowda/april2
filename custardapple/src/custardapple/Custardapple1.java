aa3ce12d9f0a18f8a9ff3508d18c19de47c5247e
Admin <Admin@DESKTOP-9833F0D>
2022-04-03 22:27:09 +0530

    1st commit of april2

---
+++ custardapple/src/custardapple/Custardapple1.java
@@ -0,0 +1,22 @@
+package custardapple;
+import java.applet.*; 
+import java.awt.*;
+public class Custardapple1 extends Applet
+{
+	public void init()
+	 {
+		}
+	 public void start()
+	 {
+		 }
+	 public void stop() { 
+		 } 
+	 public void destroy() { 
+		 } 
+	 public void paint(Graphics g)
+	 {
+	 g.setColor(Color.RED); 
+		 g.drawString("Welcome to Hubberspot!", 50, 100); 
+		 }
+	 }
+
